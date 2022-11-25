eae6ea0ee93b5b9662516f527f0178b67345eb32
wildman764 <110165081+wildman764@users.noreply.github.com>
2022-11-25 05:58:53 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -47,7 +47,7 @@ function init() {
 
 
     let loader = new THREE.GLTFLoader()
-    loader.load('../3d-obj-loader/assets/scene.gltf', function(gltf){
+    loader.load('../3d-obj-loader/assets/mustang.glb', function(gltf){
         car = gltf.scene.children[0]
         car.scale.set(0.5, 0.5, 0.5)
         scene.add(gltf.scene)
